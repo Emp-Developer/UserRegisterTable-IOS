66e392f4b0459be4e180b1a11f27b37f2c9fc297
Admin <emperor95313@gmail.com>
2020-11-03 00:05:18 +0300

    Add the Styles of Textfields of Create Use

---
+++ UserRegisterTable/extensionController.swift
@@ -0,0 +1,38 @@
+//
+//  extensionController.swift
+//  UserRegisterTable
+//
+//  Created by Admin on 11/2/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+
+    public enum OpenSansType: String {
+        case extraboldItalic = "-ExtraboldItalic"
+        case semiboldItalic = "-SemiboldItalic"
+        case semibold = "-Semibold"
+        case regular = ""
+        case lightItalic = "Light-Italic"
+        case light = "-Light"
+        case italic = "-Italic"
+        case extraBold = "-Extrabold"
+        case boldItalic = "-BoldItalic"
+        case bold = "-Bold"
+    }
+
+    static func OpenSans(_ type: OpenSansType = .regular, size: CGFloat = UIFont.systemFontSize) -> UIFont {
+        return UIFont(name: "OpenSans\(type.rawValue)", size: size)!
+    }
+
+    var isBold: Bool {
+        return fontDescriptor.symbolicTraits.contains(.traitBold)
+    }
+
+    var isItalic: Bool {
+        return fontDescriptor.symbolicTraits.contains(.traitItalic)
+    }
+
+}
