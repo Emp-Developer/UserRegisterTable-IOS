53f2d0cfd3bd6ecebe90f495c9e9158e42d16be6
Admin <emperor95313@gmail.com>
2020-11-03 00:06:03 +0300

    Add the Style

--- UserRegisterTable/ViewController.swift
+++ UserRegisterTable/ViewController.swift
@@ -9,15 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appTitleLbl: UILabel!
+    @IBOutlet weak var createNameTxt: UITextField!
+    @IBOutlet weak var createSalaryTxt: UITextField!
+    @IBOutlet weak var createAgeTxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //MARK: - Set Styles of TextField
+        self.appTitleLbl.font = UIFont.boldSystemFont(ofSize: 30)
+        
+        self.createNameTxt.attributedPlaceholder = NSAttributedString(string: "Placeholder Text", attributes: [
+            .foregroundColor: UIColor.lightGray,
+            .font: UIFont.italicSystemFont(ofSize: 18)
+        ])
+        self.createNameTxt.font = UIFont.boldSystemFont(ofSize: 20)
+        self.createNameTxt.layer.cornerRadius = 5.0
+        
+        self.createSalaryTxt.font = UIFont.boldSystemFont(ofSize: 20)
+        self.createSalaryTxt.layer.cornerRadius = 5.0
+        
+        self.createAgeTxt.font = UIFont.boldSystemFont(ofSize: 20)
+        self.createAgeTxt.layer.cornerRadius = 5.0
+        
     }
 
     // MARK: - Button Actions
-    @IBAction func onButtonClicked(_sender: UIButton) {
-        print("here")
+    @IBAction func createUserBtn(_ sender: Any) {
     }
-
+    
+    
 }
 
